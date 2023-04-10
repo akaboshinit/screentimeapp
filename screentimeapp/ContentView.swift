3da52d0df5b31e090f74e5ef5a244b1e5c135aeb
akaboshinit <akaboshinit@gmail.com>
2023-04-10 19:16:50 +0900

    Initial Commit

---
+++ screentimeapp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  screentimeapp
+//
+//  Created by redstar16 on 2023/04/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ screentimeapp/screentimeappApp.swift
@@ -0,0 +1,17 @@
+//
+//  screentimeappApp.swift
+//  screentimeapp
+//
+//  Created by redstar16 on 2023/04/10.
+//
+
+import SwiftUI
+
+@main
+struct screentimeappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
